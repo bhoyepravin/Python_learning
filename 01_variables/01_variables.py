aa1206f039dbc72f79971996e55edfa37c64987a
bhoyepravin <bhoyepravin288@gmail.com>
2025-08-31 00:01:46 +0530

    firstcommit

---
+++ 01_variables/01_variables.py
@@ -0,0 +1,22 @@
+# variable
+
+# Assigning values to variables
+a = 10
+b = 40
+print(a, b)  # Output: 10 40
+
+
+# Python optimizes memory for identical values
+a = 10
+b = 10
+
+# Using id() to show memory addresses
+print(id(a), id(b))  # Both will have the same memory address
+
+# Printing strings vs variables
+print('a')    # Output: a (string literal)
+print(a)      # Output: 10 (variable value)
+
+
+
+
